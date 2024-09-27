6b66b0b3f3eb66583cc4d1d729e5fe9d1fc167b5
Mohammad Kamran Shekh <kshekh@gmail.com>
2024-09-27 19:10:30 +0530

    code merged

--- app/components/pages/home/home.tsx
+++ app/components/pages/home/home.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Homehero from '../../ui/home-hero'
+import FlashSale from '../../ui/flash-sale'
+import PromotionalSection from '../../ui/promotional-section'
+import FurnitureZone from '../../ui/FurnitureZone'
+import AppZone from '../../ui/AppZone'
+import DigitalProduct from '../../ui/digital-product'
 
 function Homepage() {
   return (
     <main>
       <div className='container p-4 xl:px-5'>
        <Homehero />
+       <FlashSale />
+      <PromotionalSection />
+      <FurnitureZone />
+      <AppZone />
+      <DigitalProduct />
       </div>
     </main>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import Header from "./components/layout/header";
 import Navbar from "./components/layout/navbar";
 import Homepage from "./components/pages/home/home";
-import AppZone from "./components/ui/AppZone";
-import DigitalProduct from "./components/ui/digital-product";
-import FlashSale from "./components/ui/flash-sale";
-import FurnitureZone from "./components/ui/FurnitureZone";
-import PromotionalSection from "./components/ui/promotional-section";
+ 
 
 
 
@@ -17,11 +13,7 @@ export default function Home() {
  <Homepage />
  
    
-      <FlashSale />
-      <PromotionalSection />
-      <FurnitureZone />
-      <AppZone />
-      <DigitalProduct />
+      
     </>
   );
 }
